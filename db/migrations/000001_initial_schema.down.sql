ea0fa86e7f7604922667e82acd0e7293098b2cbb
charmingruby <mrrubycanal3105@gmail.com>
2024-02-21 16:41:45 -0300

    feat: changes database schema

--- db/migrations/000001_initial_schema.down.sql
+++ db/migrations/000001_initial_schema.down.sql
@@ -1,7 +1,10 @@
-DROP TABLE IF EXISTS feedbacks;
-DROP TABLE IF EXISTS feedback_categories;
-DROP TABLE IF EXISTS product_metrics;
-DROP TABLE IF EXISTS products;
+DROP INDEX IF EXISTS subscribers_email_uindex;
+DROP INDEX IF EXISTS accounts_email_uindex;
+
+DROP TABLE IF EXISTS subscribers;
 DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS mailing_lists;
+DROP TABLE IF EXISTS newsletters;
+DROP TABLE IF EXISTS newsletters_tags;
 
 DROP EXTENSION IF EXISTS "uuid-ossp";
--- db/migrations/000001_initial_schema.up.sql
+++ db/migrations/000001_initial_schema.up.sql
@@ -2,60 +2,65 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS accounts (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    
     name varchar NOT NULL,
     last_name varchar NOT NULL,
     email varchar NOT NULL,
-    role varchar NOT NULL,
     avatar_url varchar NOT NULL,
     password varchar NOT NULL,
+
     created_at timestamp DEFAULT now() NOT NULL,
     updated_at timestamp DEFAULT now() NOT NULL   
 );
 
+CREATE TABLE IF NOT EXISTS subscribers (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+
+    name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    email varchar NOT NULL,
+    
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL   
+);
+
+CREATE TABLE IF NOT EXISTS newsletters_tags (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    title varchar NOT NULL,
+    description varchar NOT NULL,
+
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS newsletters (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    
+    title varchar NOT NULL,
+    subject varchar NOT NULL,
+    content text NOT NULL,
+    status varchar NOT NULL,
+    
+    tag_id UUID REFERENCES newsletters_tags (id) NOT NULL,
+    author_id UUID REFERENCES accounts (id) NOT NULL,
+
+    published_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS mailing_lists (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+    status varchar NOT NULL,
+    
+    recipient_id UUID REFERENCES subscribers (id) NOT NULL,
+    newsletters_id UUID REFERENCES newsletters (id) NOT NULL,
+
+    created_at timestamp DEFAULT now() NOT NULL,
+    updated_at timestamp DEFAULT now() NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS subscribers_email_uindex
+    ON subscribers (email);
+
 CREATE UNIQUE INDEX IF NOT EXISTS accounts_email_uindex
     ON accounts (email);
-
-
-CREATE TABLE IF NOT EXISTS products (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
-    name varchar NOT NULL,
-    description varchar NOT NULL,
-    logo_url varchar NOT NULL,
-
-    account_id UUID REFERENCES accounts (id) NOT NULL,
-
-    created_at timestamp DEFAULT now() NOT NULL,
-    updated_at timestamp DEFAULT now() NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS product_metrics (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
-    total_feedbacks integer NOT NULL,
-    amount_of_stars integer NOT NULL,
-
-    product_id UUID REFERENCES products (id) NOT NULL,
-
-    created_at timestamp DEFAULT now() NOT NULL,
-    updated_at timestamp DEFAULT now() NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS feedback_categories (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
-    name varchar NOT NULL,
-    description varchar NOT NULL,
-    created_at timestamp DEFAULT now() NOT NULL,
-    updated_at timestamp DEFAULT now() NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS feedbacks (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
-    rate integer NOT NULL,
-    comment text NOT NULL,
-
-    feedback_category_id UUID REFERENCES feedback_categories (id) NOT NULL,
-    product_id UUID REFERENCES products (id) NOT NULL,
-    account_id UUID REFERENCES accounts (id) NOT NULL,
-
-    created_at timestamp DEFAULT now() NOT NULL,
-    updated_at timestamp DEFAULT now() NOT NULL
-);
